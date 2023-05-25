9db6e0b657b02ffdfbce5372069f710bef9a1758
Roberto Castro <robertorcastro@ua.pt>
2023-05-25 15:35:05 +0100

    FINAL

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,2 @@
-import images from "./images";
-import icons from "./icons";
 import { COLORS, FONT, SIZES, SHADOWS } from "./theme";
-
-export { images, icons, COLORS, FONT, SIZES, SHADOWS };
+export { COLORS, FONT, SIZES, SHADOWS };
